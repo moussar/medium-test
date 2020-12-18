c9529e7cb249645401c2dda29c2a3d4fec8cf7f7
“Mouhssine AREGU <mouhssine.aregu@gmail.com>
2020-12-18 13:54:25 +0100

    initial fronted & add SSR

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
