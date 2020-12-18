785e482e54b95465db18ae05cd0564ad690809af
“Mouhssine AREGU <mouhssine.aregu@gmail.com>
2020-12-18 14:03:47 +0100

    generate components & build HTML/CSS

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './components/article/article.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
+import { CommentComponent } from './components/comment/comment.component';
+import { ReactionComponent } from './components/reaction/reaction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ArticleComponent,
+    LoginComponent,
+    RegisterComponent,
+    SearchComponent,
+    HomeComponent,
+    CommentComponent,
+    ReactionComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/reaction/reaction.component.ts
@@ -0,0 +1,9 @@
+import { Component, } from '@angular/core';
+
+@Component({
+  selector: 'app-reaction',
+  templateUrl: './reaction.component.html',
+  styleUrls: ['./reaction.component.scss']
+})
+export class ReactionComponent {
+}
